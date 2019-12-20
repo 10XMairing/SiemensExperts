4c2e4fa3f145506f30dc8551cc0dba12104498c2
10xmairing <10xpumba@gmail.com>
2019-12-20 09:51:37 +0530

    changed baseurl

--- app/src/main/java/com/github/tenx/xservices/config/Config.java
+++ app/src/main/java/com/github/tenx/xservices/config/Config.java
@@ -3,6 +3,8 @@ package com.github.tenx.xservices.config;
 public class Config {
 
 
-    public static final String REST_BASE_URL="http://10.0.2.2:3000";
+//    public static final String REST_BASE_URL="http://10.0.2.2:3000";
+public static final String REST_BASE_URL="http://agtech.xydev.io";
+
 
 }
